1036c9c56a84c285317557f09d8651394ecbff7b
jose-petit <joselocko@gmail.com>
2019-02-08 03:08:50 -0400

    PruebaInicial

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
+})
+export class AppComponent {
+  public appMenu = [
+    {title: 'Test1', url: '/test1', icon:'list'},
+    {title: 'Test2', url: '/test2', icon:'add'},
+    {title: 'Test3', url: '/test3', icon:'trash'},
+  ];
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+}
